33b9e1f1f659d5a236745e9dc57962f097a73277
Willian Duarte <williandasilvaduarte10@gmail.com>
2025-10-20 11:34:09 -0300

    Projeto (Desenvolvido em 2024)

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  //if(html.classList.contains('light')){
+  //html.classList.remove('light')
+  //}else{
+  //html.classList.add('light')
+  //}
+  html.classList.toggle("light")
+
+  /*const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/will-light.png")
+  } else {
+    img.setAttribute("src", "./assets/will.png")
+  }
+
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "dwewda")
+  } else {
+    img.setAttribute("alt", "aadawds")
+  }*/
+}
